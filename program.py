56a55f7769602d47c57a3090f06632c3594dbacc
calebhaegg <caleb.hagg@elev.ga.ntig.se>
2019-09-18 13:28:46 +0200

    kommentarerna har uppdaterats från föra programkursen 18/09

--- program.py
+++ program.py
@@ -10,17 +10,18 @@ while vaken == "n":
 # Duschen
 print("Du går direkt in i duschen.")
 print("Du har råkat använda brödrosten i duschen.")
-# Skapa if, elif och else funktionerna
+# Skapa if, elif och else funktionerna.
 duscha = input("Flyttar du på brödrosten? [y/n] ").lower()
 if duscha == "n":
     print("Du får en elektriskt shock och det är slutet")
     exit()
 elif duscha == "y":
-    print("Friskt vatten kan sköljas över dig och kan väcka upp dig.")
+    print("Friskt vatten kan sköljas över dig och kan väcka upp dig.") # Här skapar man en print-funktion som skriver ut koden med parenteser och variabler används alltid med str-funktionen.
 else:
     print("DOES NOT COMPUTE")
 
-season = False
+# Årstiderna
+season = False # Så här kodar en boo-funktionen som framställer antingen sant eller falskt variabler.
 while season == False:
     season = input("Vilken årstied är det [vår, vinter, sommar, höst]").lower()
     if season == "vår" or season == "vinter" or season == "höst":
@@ -30,4 +31,5 @@ while season == False:
         print("Det här ska vara trevligt väder att sola!")
     else:
         season = False
+# Ta fram while-funktionen så att dataprocessen ska uppdatereas och upprepas samtidigt och använd boolsk (true och false)datatypen till hjälp också.
 
